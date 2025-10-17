f81e448d1f261e570a6db288cfdfe1148fc3515a
joyhz <jhzhu@scu.edu>
2025-10-17 12:09:36 -0700

    added names + changed commands and cases

--- bagtest.cxx
+++ bagtest.cxx
@@ -1,5 +1,6 @@
 /*
- * Sin-Yaw Wang <swang24@scu.edu>
+ * Names: Yuto Fraley, Theodore Griffin - Jones, Michael Tiburcio, Joy Zhu
+ * Emails: yfraley@scu.edu, tgriffinjones@scu.edu, mtiburcio@scu.edu, jhzhu@scu.edu
  * Test program
  */
 #include <iostream>
@@ -11,37 +12,55 @@
 
 using namespace std;
 using namespace csen79;
-enum Command { PUSH = 'P', POP = 'O', PRINT = 'D', QUIT = 'Q' };
+enum Command { ENQ = 'E', DEQ = 'D', PRINT = 'P', QUIT = 'Q' };
 
-int main(void) {
+int main(int argc, char *argv[]) {
     Bag bag;
     string line;
     bool quit = false;
-    while (!quit && std::getline(cin,line)) {
-        switch (line[0]) {
-        case PUSH: {
-            int data;
-            try {
-                stringstream ss(line.substr(1));
-                ss >> data;
-            } catch (const out_of_range &e) {
-                cerr << "Data out of range" << endl;
-                continue;
-            }
-            bag.push(data);
+
+    istream *input; // Pointer to input stream
+    ifstream inputFile;
+    if (argc == 1) {
+        cout << "No file provided, reading from standard input." << endl;
+        input = &cin;   // input points to standard input
+    }
+    else {
+        inputFile.open(argv[1]);
+        cout << "Reading from file: " << argv[1] << endl;
+        if (!inputFile) {
+            cerr << "Error opening file: " << argv[1] << endl;
+            exit(1);
         }
-            break;
-        case POP:
-        case PRINT:
-            break;
-        case QUIT:
-            quit = true;
-            break;
-        default:
-            cerr << "Unknown command: " << line << endl;
-            break;
-        }
+        input = &inputFile; // input points to the file stream
     }
     
+    while (!quit && getline(*input,line)) {
+        if (line.empty()) 
+            continue;   // skip empty lines
+        switch (line[0]) {
+            case ENQ: {
+                int data;
+                try {
+                    stringstream ss(line.substr(1));
+                    ss >> data;
+                } catch (const out_of_range &e) {
+                    cerr << "Data out of range" << endl;
+                    continue;
+                }
+                bag.push(data);
+            }
+                break;
+            case DEQ:
+            case PRINT:
+                break;
+            case QUIT:
+                quit = true;
+                break;
+            default:
+                cerr << "Unknown command: " << line << endl;
+                break;
+        }
+    }
     return EXIT_SUCCESS;
 }
