876bb7f740d49317de29d8c1e32c8afc2012a49d
joyhz <jhzhu@scu.edu>
2025-10-17 12:04:06 -0700

    added names + helper functions

--- bag.cxx
+++ bag.cxx
@@ -1,5 +1,6 @@
 /*
- * Sin-Yaw Wang <swang24@scu.edu>
+ * Names: Yuto Fraley, Theodore Griffin - Jones, Michael Tiburcio, Joy Zhu
+ * Emails: yfraley@scu.edu, tgriffinjones@scu.edu, mtiburcio@scu.edu, jhzhu@scu.edu
  * recipe for containers using C-style array
  */
 #include <iostream>
@@ -38,19 +39,42 @@ namespace csen79 {
     }
 
 
-    // simple asssess functions
+    // simple access functions
     // replace them with appropriate ones for assignments
     const Bag::Data &Bag::getData(const int i) const {
         if (i < 0 || i >= DATASIZE)
-            throw std::out_of_range(std::string("index out of range"));
+            throw std::out_of_range(std::string("Index out of range"));
         return data[i];
     };
-    void Bag::setData(const int i, const Data &d) {
-        if (i < 0 || i >= DATASIZE)
-            throw std::out_of_range(std::string("index out of range"));
-        data[i] = (Data) d;
+
+    bool Bag::isEmpty() const {
+        return top < 0;
     }
-    void Bag::push(const Data &) {};
-    Bag::Data Bag::pop() {return 0;};
-    void Bag::print() const {};
+
+    bool Bag::isFull() const {
+        return size() >= DATASIZE;  // Can't compare top w/ DATASIZE, compares int w/ size_t (unsigned int) --> have to convert top to size_t to compare
+    }
+
+    std::size_t Bag::size() const {
+        return static_cast<std::size_t>(top + 1);
+    }
+
+    void Bag::setData(const int i, const Data &d) {
+        if (i < 0 || i >= DATASIZE)    // No issue comparing here; neg int --> throw, pos int --> simple comparison
+            throw std::out_of_range("Index out of range");
+        data[i] = d;
+    }
+
+    void Bag::enQ(const Data &) {};
+    Bag::Data Bag::deQ() {return 0;};
+    
+    void Bag::print() const {
+        if(isEmpty())
+            std::cout << "Queue is empty" << std::endl;
+        /*
+        for (int i = top; i >= 0; i--) {
+            std::cout << data[i] << std::endl; // Printing from the top -> down
+        */
+        }
+    };
 }
